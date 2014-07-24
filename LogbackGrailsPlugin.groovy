ad761d4cc4787841023533bec16688d398af0825
Alvaro Sanchez-Mariscal <alvaro.sanchez@odobo.com>
2014-07-24 17:21:21 +0200

    Add support for Grails 2.4

--- LogbackGrailsPlugin.groovy
+++ LogbackGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import org.slf4j.bridge.SLF4JBridgeHandler
 
 class LogbackGrailsPlugin {
 
-	String version = '0.3.1'
+	String version = '0.3.2-SNAPSHOT'
 	String grailsVersion = '2.0 > *'
 	String title = 'Logback Plugin'
 	String author = 'Burt Beckwith'
